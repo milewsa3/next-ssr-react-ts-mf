5c08eb54e90fe2c3fb3911c96ddac20c2c808842
Adrian <adrian.milewski@bluesoft.com>
2023-06-27 14:09:25 +0200

    Init commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
+const nextConfig = {
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: 'host',
+          remotes: {
+            remote: 'remote@http://localhost:3001/remote.js',
+          },
+          filename: 'static/chunks/remoteEntry.js',
+        }),
+      );
+    }
+
+    return config;
+  },
+};
 
 module.exports = nextConfig
