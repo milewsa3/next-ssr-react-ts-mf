513e25b88fceb8ba5477511aab261d6a1ff0f8f8
Adrian <adrian.milewski@bluesoft.com>
2023-06-27 14:33:12 +0200

    Add working example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 // host/pages/index.js
 import dynamic from 'next/dynamic';
 
+// @ts-ignore  
 const Nav = dynamic(() => import('remote/Nav'), { ssr: false });
 
 export default function HomePage() {
